8ba0434b81733a2afb4671a7fd560ae91aa27af0
Benjohnson Hembram <47394500+benjanson@users.noreply.github.com>
2020-03-07 00:49:26 +0530

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,7 @@
+
+print('\n\nEnter the number to identify weather the number is even or odd')
+p=int(input(''))            #number to be inserted
+if p%2==0:
+    print('even')           #Display if its even number
+else:
+    print('odd')            #Display if its odd number
