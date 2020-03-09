32b6baa419e49fd6e75b5947a4c2d1f7bf9cabab
Benjohnson Hembram <47394500+benjanson@users.noreply.github.com>
2020-03-10 01:48:32 +0530

    Update List_Mutability.py

--- List_Mutability.py
+++ List_Mutability.py
@@ -1,7 +1,18 @@
-#to show list is mutable or not
+#to show list and Dict are mutable or not
 
-#creating a list
+# list
 Fruits = ["Apple","Banana","Mango","Strawberry","Watermelon"]
+print(id(Fruits))
+print(Fruits)
+
+Vegetables = Fruits
+Vegetables[1]='Orange'
+
+print(id(Vegetables))
+print(Vegetables)
+
+#dictonary
+Fruits = {1:"Apple",2:"Banana",3:"Mango",4:"Strawberry",5:"Watermelon"}
 print(id(Fruits))
 print(Fruits)
 
